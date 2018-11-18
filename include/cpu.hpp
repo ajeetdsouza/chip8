739dd91bc726e1aac455bf399149e3b1c26ac09d
Ajeet D'Souza <98ajeet@gmail.com>
2018-11-19 01:26:14 +0530

    Initial commit

---
+++ include/cpu.hpp
@@ -0,0 +1,140 @@
+#ifndef INCLUDE_CPU_H_
+#define INCLUDE_CPU_H_
+
+#include <array>
+#include <bitset>
+#include <iostream>
+#include <string>
+
+
+class Cpu {
+private:
+    std::array<std::bitset<64>, 32> frame_buf_; // frame buffer
+    std::array<uint8_t, 4096> mem;              // addressable memory
+    std::array<uint16_t, 16> stack;             // stack memory
+
+    std::array<uint8_t, 16> v; // register V0 - VF
+    uint16_t i;                // "I" register
+
+    uint8_t sp;  // stack pointer
+    uint16_t pc; // program counter
+
+    uint8_t delay_timer;
+    uint8_t sound_timer;
+
+    uint16_t keys; // store key presses
+
+    static constexpr std::array<uint8_t, 16 * 5> font_set{
+            0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+            0x20, 0x60, 0x20, 0x20, 0x70, // 1
+            0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+            0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+            0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+            0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+            0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+            0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+            0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+            0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+            0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+            0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+            0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+            0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+            0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+            0xF0, 0x80, 0xF0, 0x80, 0x80  // F
+    };
+
+
+    inline uint16_t get_opcode() const;
+
+    void run_opcode(uint16_t);
+
+    void run_timers();
+
+    void opcode_0x0NNN(uint16_t);
+
+    void opcode_0x00EE(uint16_t);
+
+    void opcode_0x00E0(uint16_t);
+
+    void opcode_0x1NNN(uint16_t);
+
+    void opcode_0x2NNN(uint16_t);
+
+    void opcode_0x3XNN(uint16_t);
+
+    void opcode_0x4XNN(uint16_t);
+
+    void opcode_0x5XY0(uint16_t);
+
+    void opcode_0x6XNN(uint16_t);
+
+    void opcode_0x7XNN(uint16_t);
+
+    void opcode_0x8XY0(uint16_t);
+
+    void opcode_0x8XY1(uint16_t);
+
+    void opcode_0x8XY2(uint16_t);
+
+    void opcode_0x8XY3(uint16_t);
+
+    void opcode_0x8XY4(uint16_t);
+
+    void opcode_0x8XY5(uint16_t);
+
+    void opcode_0x8XY6(uint16_t);
+
+    void opcode_0x8XY7(uint16_t);
+
+    void opcode_0x8XYE(uint16_t);
+
+    void opcode_0x9XY0(uint16_t);
+
+    void opcode_0xANNN(uint16_t);
+
+    void opcode_0xBNNN(uint16_t);
+
+    void opcode_0xCXNN(uint16_t);
+
+    void opcode_0xDXYN(uint16_t);
+
+    void opcode_0xEX9E(uint16_t);
+
+    void opcode_0xEXA1(uint16_t);
+
+    void opcode_0xFX07(uint16_t);
+
+    void opcode_0xFX0A(uint16_t);
+
+    void opcode_0xFX15(uint16_t);
+
+    void opcode_0xFX18(uint16_t);
+
+    void opcode_0xFX1E(uint16_t);
+
+    void opcode_0xFX29(uint16_t);
+
+    void opcode_0xFX33(uint16_t);
+
+    void opcode_0xFX55(uint16_t);
+
+    void opcode_0xFX65(uint16_t);
+
+public:
+    Cpu();
+
+    const decltype(frame_buf_) &frame_buf; // read-only frame buffer
+
+    void load(const std::string &);
+
+    void cycle();
+
+    void press_key(int);
+
+    void release_key(int);
+
+    bool redraw;
+    bool beep;
+};
+
+#endif // INCLUDE_CPU_H_
---
+++ src/main.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+#include <unordered_map>
+
+#include <SDL2/SDL.h>
+
+#include "../include/cpu.hpp"
+
+const std::unordered_map<SDL_Keycode, int> keymap = {
+        {SDLK_x, 0x0},
+        {SDLK_1, 0x1},
+        {SDLK_2, 0x2},
+        {SDLK_3, 0x3},
+        {SDLK_q, 0x4},
+        {SDLK_w, 0x5},
+        {SDLK_e, 0x6},
+        {SDLK_a, 0x7},
+        {SDLK_s, 0x8},
+        {SDLK_d, 0x9},
+        {SDLK_z, 0xA},
+        {SDLK_c, 0xB},
+        {SDLK_4, 0xC},
+        {SDLK_r, 0xD},
+        {SDLK_f, 0xE},
+        {SDLK_v, 0xF}
+};
+
+int main(int argc, char *argv[]) {
+    Cpu cpu;
+
+    if (argc != 2) {
+        std::cout << "Usage: ./chip8 <ROM file>" << std::endl;
+	return 1;
+    }
+    cpu.load(argv[1]);
+
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        std::cerr << "Initialization failed!" << std::endl;
+        return 1;
+    }
+
+    auto window = SDL_CreateWindow("chip8", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+                              640, 320, SDL_WINDOW_SHOWN);
+    if (window == nullptr) {
+        std::cerr << "Could not create window: " << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return 1;
+    }
+
+    auto renderer = SDL_CreateRenderer(window, 0, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    SDL_RenderSetLogicalSize(renderer, 64, 32);
+
+    SDL_Event event;
+
+    while (true) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                SDL_Quit();
+                return 0;
+            }
+
+            if (event.type == SDL_KEYDOWN || event.type == SDL_KEYUP) {
+                const auto key = keymap.find(event.key.keysym.sym);
+                if (key == keymap.end())
+                    break;
+
+                else if (event.type == SDL_KEYDOWN)
+                    cpu.press_key(key->second);
+
+                else if (event.type == SDL_KEYUP)
+                    cpu.release_key(key->second);
+            }
+        }
+
+        cpu.cycle();
+
+        if (cpu.redraw) {
+            SDL_RenderClear(renderer);
+            SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+
+            for (auto y_i = 0; y_i < cpu.frame_buf.size(); ++y_i)
+                for (auto x_i = 0; x_i < cpu.frame_buf[y_i].size(); ++x_i)
+                    if (cpu.frame_buf[y_i][x_i])
+                        SDL_RenderDrawPoint(renderer, x_i, y_i);
+
+            SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0xFF);
+            SDL_RenderPresent(renderer);
+
+            cpu.redraw = false;
+        }
+
+        if (cpu.beep) {
+            std::cout << "beep" << std::endl;
+
+            cpu.beep = false;
+        }
+    }
+}
